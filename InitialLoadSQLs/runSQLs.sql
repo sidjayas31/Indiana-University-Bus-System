3cf97c0471d9aa04d9633ac4ba30fcfaa8c91a3d
reladric <hari.sarvothaman@gmail.com>
2015-11-28 22:54:19 -0500

    CHanged sql

--- InitialLoadSQLs/runSQLs.sql
+++ InitialLoadSQLs/runSQLs.sql
@@ -29,17 +29,134 @@ FROM (	SELECT i.from_id, to_id,time_id, bus_id,r.`Route ID`, when_time
 									),'4:00:00' ) < subtime( wk1.`Clock Out`,
 									'4:00:00')) 
 		);
+CREATE TABLE S_SPRING_RUNS_DEMO 
+SELECT Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id , 
+	concat(DATE(when_time),concat('~',concat(SUBSTRING(Route,1,1),concat('~',
+	concat(TripID,concat('~',Route)))))) 'id', concat(SUBSTRING(Route,1,1),
+	concat('~',concat(TripID,concat('~',Route)))) 'integration_key' ,
+	case when from_id = to_id then 0 else 1 end travel_flag
+FROM (	SELECT *, 
+			@row_num := IF(date(when_time) !=@prev_date,@row_num:=0,IF( `to_id` = 67 
+			AND `from_id` =67 , 
+			@row_num+1, 
+			@row_num)) TripID, IF(`to_id` = 67 
+			AND `from_id` =67 , 
+			@stop_order:=0, IF (`from_id`=`to_id`, 
+			@stop_order:= 
+			@stop_order +1, 
+			@stop_order:= 
+			@stop_order)) stop_order ,
+			@prev_date:=date(when_time)
+		FROM SPRING_RUNS_TEMP 
+			JOIN (	SELECT 
+						@row_num :=0 
+			)
+			r 
+			JOIN (	SELECT 
+						@stop_order:=0 
+			)
+			s 
+		WHERE Route LIKE '_-A_.%' 
+		ORDER BY `Date`, Route,bus_id, `when_Time` 
+	)
+	a 
+UNION 
+SELECT Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id , 
+	concat(DATE(when_time),concat('~',concat(SUBSTRING(Route,1,1),concat('~',
+	concat(TripID,concat('~',Route)))))) 'id', concat(SUBSTRING(Route,1,1),
+	concat('~',concat(TripID,concat('~',Route)))) 'integration_key' ,
+	case when from_id = to_id then 0 else 1 end travel_flag 
+FROM (	SELECT *, 
+			IF(date(when_time) !=@prev_date,@row_num:=0,@row_num := IF(`to_id` = 24 
+			AND `from_id` =24 , 
+			@row_num+1, 
+			@row_num)) TripID, IF(`to_id` = 24 
+			AND `from_id` =24 , 
+			@stop_order:=0, IF (`from_id`=`to_id`, 
+			@stop_order:= 
+			@stop_order +1, 
+			@stop_order:= 
+			@stop_order)) stop_order ,
+			@prev_date:=date(when_time)
+		FROM SPRING_RUNS_TEMP 
+			JOIN (	SELECT 
+						@row_num :=0 
+			)
+			r 
+			JOIN (	SELECT 
+						@stop_order:=0 
+			)
+			s 
+		WHERE Route LIKE '_-B_.%' 
+		ORDER BY `Date`, Route,bus_id, `when_Time` 
+	)
+	b 
+UNION 
+SELECT Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id , 
+	concat(DATE(when_time),concat('~',concat(SUBSTRING(Route,1,1),concat('~',
+	concat(TripID,concat('~',Route)))))) 'id', concat(SUBSTRING(Route,1,1),
+	concat('~',concat(TripID,concat('~',Route)))) 'integration_key'  ,
+	case when from_id = to_id then 0 else 1 end travel_flag
+FROM (	SELECT *, 
+			IF(date(when_time) !=@prev_date,@row_num:=0,@row_num := IF(`to_id` = 62 
+			AND `from_id` =62 , 
+			@row_num+1, 
+			@row_num) )TripID, IF(`to_id` = 62 
+			AND `from_id` =62 , 
+			@stop_order:=0, IF (`from_id`=`to_id`, 
+			@stop_order:= 
+			@stop_order +1, 
+			@stop_order:= 
+			@stop_order)) stop_order ,
+			@prev_date:=date(when_time)
+		FROM SPRING_RUNS_TEMP 
+			JOIN (	SELECT 
+						@row_num :=0 
+			)
+			r 
+			JOIN (	SELECT 
+						@stop_order:=0 
+			)
+			s 
+		WHERE Route LIKE '_-E_.%' 
+		ORDER BY `Date`, Route,bus_id, `when_Time` 
+	)
+	C 
+UNION 
+SELECT Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id , 
+	concat(DATE(when_time),concat('~',concat(SUBSTRING(Route,1,1),concat('~',
+	concat(TripID,concat('~',Route)))))) 'id', concat(SUBSTRING(Route,1,1),
+	concat('~',concat(TripID,concat('~',Route)))) 'integration_key'  ,
+	case when from_id = to_id then 0 else 1 end travel_flag
+FROM (	SELECT *, 
+			IF(date(when_time) !=@prev_date,@row_num:=0,@row_num := IF(`to_id` = 76 
+			AND `from_id` =76 , 
+			@row_num+1, 
+			@row_num)) TripID, IF(`to_id` = 76 
+			AND `from_id` =76 , 
+			@stop_order:=0, IF (`from_id`=`to_id`, 
+			@stop_order:= 
+			@stop_order +1, 
+			@stop_order:= 
+			@stop_order)) stop_order ,
+			@prev_date:=date(when_time)
+		FROM SPRING_RUNS_TEMP 
+			JOIN (	SELECT 
+						@row_num :=0 
+			)
+			r 
+			JOIN (	SELECT 
+						@stop_order:=0 
+			)
+			s 
+		WHERE Route LIKE '_-X_.%' 
+		ORDER BY `Date`, Route,bus_id, `when_Time` 
+	)
+	d;
 
-CREATE TABLE S_SPRING_RUNS
-select Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id from (select *,@row_num := IF(`to_id` = 39 and `from_id` =39 ,@row_num+1,@row_num) TripID, IF(`to_id` = 39  and `from_id` =39 ,@stop_order:=0,@stop_order:=@stop_order +1) stop_order  from SPRING_RUNS_TEMP JOIN ( SELECT @row_num :=0)r JOIN (SELECT @stop_order:=0)s where Route like '_-A_.%'order by `Date`, Route,bus_id, `when_Time`)a UNION
-select Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id from (select *,@row_num := IF(`to_id` = 25 and `from_id` =25 ,@row_num+1,@row_num) TripID, IF(`to_id` = 25  and `from_id` =25 ,@stop_order:=0,@stop_order:=@stop_order +1) stop_order  from SPRING_RUNS_TEMP JOIN ( SELECT @row_num :=0)r JOIN (SELECT @stop_order:=0)s where Route like '_-B_.%'order by `Date`, Route,bus_id, `when_Time`)b UNION
-select Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id from (select *,@row_num := IF(`to_id` = 48 and `from_id` =48 ,@row_num+1,@row_num) TripID, IF(`to_id` = 48  and `from_id` =48 ,@stop_order:=0,@stop_order:=@stop_order +1) stop_order   from SPRING_RUNS_TEMP JOIN ( SELECT @row_num :=0)r JOIN (SELECT @stop_order:=0)s where Route like '_-E_.%'order by `Date`, Route,bus_id, `when_Time`)c UNION
-select Route, `Date`, TripID, from_id,to_id, when_time, stop_order,bus_id from (select *,@row_num := IF(`to_id` = 68 and `from_id` =68 ,@row_num+1,@row_num) TripID, IF(`to_id` = 68  and `from_id` =68 ,@stop_order:=0,@stop_order:=@stop_order +1) stop_order   from SPRING_RUNS_TEMP JOIN ( SELECT @row_num :=0)r JOIN (SELECT @stop_order:=0)s where Route like '_-X_.%'order by `Date`, Route,bus_id, `when_Time`)d;
 
-
-
-
-
+create view S_SPRING_RUNS_WITH_STOP as select s.*, t1.Stop as `from`, t2.Stop as `to` from S_SPRING_RUNS_DEMO s, DBMAP_StopID t1, DBMAP_StopID t2 where
+s.from_id = t1.ID and s.to_id=t2.ID;
 
 
 select str_to_date(subtime(date_format(now(),'%h:%i:%S %p'),'2:00:00'),'%h:%i:%S');
